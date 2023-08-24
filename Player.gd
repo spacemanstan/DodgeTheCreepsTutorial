381d8e887f54048e5c476f03561fa392f9202af6
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-08-24 14:18:38 -0600

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,57 @@
+extends Area2D
+
+
+signal hit
+
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # player movement vec
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		get_node("AnimatedSprite2D").play()
+	else:
+		get_node("AnimatedSprite2D").stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0	
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	hide() # disappear on hit
+	hit.emit()
+	# set_deferred() tells Godot to wait to disable the shape until it's safe to do so.
+	$CollisionShape2D.set_deferred("disabled", true) 
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
---
+++ README.md
@@ -0,0 +1,2 @@
+# DodgeTheCreepsTutorial
+ Godot tutorial for a basic 2D game
